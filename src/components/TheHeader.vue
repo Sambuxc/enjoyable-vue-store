f57fd845b0956d16bd5050c0fd796f31f0787ed3
sambuxc <samwbru@gmail.com>
2025-07-02 16:58:05 +0100

    - adds username to header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,9 @@
 <script setup>
 // imports
 import CartWidget from './CartWidget.vue';
+// stores
+import { useAuthUserStore } from '@/stores/AuthUserStore';
+const authUserStore = useAuthUserStore();
 </script>
 
 <template>
@@ -9,6 +12,11 @@ import CartWidget from './CartWidget.vue';
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div class="flex gap-4">
+      <i class="fas fa-user">
+        <p v-show="authUserStore.username">{{ authUserStore.username }}</p>
+      </i>
+      <CartWidget/>
+    </div>
   </header>
 </template>
