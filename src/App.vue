3a1e43ff1059192c4e5c5c4bdc19edc01d178b46
sambuxc <samwbru@gmail.com>
2025-07-09 07:44:45 +0100

    - adds docs and expands on the action

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,25 @@ const cartStore = useCartStore()
 const productStore = useProductStore()
 productStore.fill()
 
-cartStore.$onAction (({ name, args, after, onError}) => {
+// Subscribers
+// https://pinia.vuejs.org/core-concepts/actions.html#Subscribing-to-actions
+
+/**
+ * When an action is made within the cart store
+ */
+cartStore.$onAction (
+  ({
+    name, // name of the action
+    store, // store instance, same as `someStore`
+    args, // array of parameters passed to the action
+    after, // hook after the action returns or resolves
+    onError, // hook if the action throws or rejects  
+  }) => {
+    // a shared variable for this specific action call
+    const startTime = Date.now()
+    // this will trigger before an action on `store` is executed
+    console.log(`Start "${name}" with params [${args.join(', ')}].`)
+
   if (name === 'addItems') {
     after(() => {
       // This will run after the action is completed
@@ -18,7 +36,21 @@ cartStore.$onAction (({ name, args, after, onError}) => {
       // or if onError, then log the error somewhere
     })
   }
-})
+
+  after(() => {
+      console.log(
+        `Finished "${name}" after ${
+          Date.now() - startTime
+        }ms.`
+      )
+  })
+  // this will trigger if the action throws or returns a promise that rejects
+  onError((error) => {
+    console.warn(
+      `Failed "${name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+    )
+  })
+}, true) // true keeps this subscriber alive if component unmounts
 
 </script>
 
